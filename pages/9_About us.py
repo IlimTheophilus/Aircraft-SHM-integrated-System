63f8beca3a75a288014cca38987818132e38840f
IlimTheophilus <ilimtheophilus@gmail.com>
2026-01-28 16:47:30 +0100

    Create 9_About us.py

---
+++ pages/9_About us.py
@@ -0,0 +1,151 @@
+import streamlit as st
+from PIL import Image
+
+
+# Title
+st.title(" Aerospace Intelligent Systems (AIS) Technologies ")
+
+
+# Image
+
+
+image_path = "images/ASHMIS img.png"
+img = Image.open(image_path)
+
+# Fit image to the column width
+st.image(img, caption="Sample Image", use_container_width=True)
+
+
+
+# Text
+
+# Header
+st.subheader(" AeroIntel Systems (AIS) Technologies ")
+
+st.subheader(" Building the Next Generation of Intelligence Systems ")
+
+# Write
+st.write("""Aerospace Intelligent Systems (AIS) Technologies is an Aerospace and Defense technology company dedicated to the design and development of intelligent, mission-critical systems for aviation, space,
+and national defense applications.
+We build advanced aerospace systems that integrate engineering, sensing, data intelligence, and autonomy to support aircraft,systems platforms, and defense infrastructure across their full operational lifecycle. 
+Our focus is not a single product or domain, but the creation of scalable aerospace and defense capabilities that strengthen operational effectiveness, technological independence, and strategic readiness.
+          Aerospace Intelligent Systems (AIS) Technologies recognizes aerospace technology as a strategic National asset.
+Our work contributes to:
+•	Enhanced Aircraft Maintenance and platform readiness
+•	Reduced dependence on foreign diagnostic and maintenance systems
+•	Stronger National Defense capabilities
+•	Low-Cost Indigenous Aerospace technology development
+By building local expertise and scalable systems, we aim to support National defense readiness while competing globally.
+________________________________________
+
+         """)
+
+
+# Image
+
+
+image_path = "images/fighter jet pic.jpg"
+img = Image.open(image_path)
+
+# Fit image to the column width
+st.image(img, caption="Sample Image", use_container_width=True)
+
+
+st.write("""ASHMIS is an integrated, lowcost Airframe Structural Health Monitoring System that leverages computer vision, thermal
+         imaging and Machine learning to automatically detect structural defects in aircraft components such as wings, fuselage and 
+         tail sections. This module enables the acquisition, processing, and analysis of visual and thermal images to identify potential 
+         issues such as cracks, delamination, and corrosion, facilitating timely maintenance and ensuring flight safety. Using a hybrid 
+         detection architecture, combining thermal gradient analysis, pixel level defect segmentation and ML-driven pattern recognition, 
+         ASHMIS evaluates each snapshot for indications of structural degradation, hotspots, material discontinuities, or other early-stage
+         failutre signatures.""")
+
+
+# --- HEADER SECTION ---
+st.markdown("""
+    <style>
+    .hero {
+        text-align: center;
+        padding: 50px 20px;
+        background: linear-gradient(135deg,#0369a1, #0ea5e9);
+        color: yellow;
+        border-radius: 10px;
+    }
+    .hero h1 {
+        font-size: 3em;
+        font-weight: 800;
+    }
+    .hero p {
+        font-size: 1.3em;
+        color: #e0f2fe;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+<div class="hero">
+    <h1>Drone Vision AI</h1>
+    <p>Real-time Aircraft Wing Health Monitoring and Defect Detection powered by Artificial Intelligence </p>
+</div>
+""", unsafe_allow_html=True)
+
+
+st.write(" Aircraft's Structural Integrity is vital to flight safety and maintenance cost efficiency. Conventional inspection methods although effective, are expensive and labour intensive, limiting their frequent use in developing Aerospace enviroments such as Nigeria. This System Integrates Thermal imaging and Visual "
+         "Imaging to detect cracks, delamination and other structural defects.")
+
+
+st.write(" Such an Application is designed to demonstrate that affordable imaging and AI technologies can be integrated to acheive reliable structural health assessment.")
+
+# Simple input
+name = st.text_input(
+    "Enter your Name Below to join the team, and be part of something special to come:")
+
+# Button
+if st.button("submit name"):
+    st.write(
+        f"Hello, {name}! Your name has been submitted, standby for processing 😄")
+
+# Slider example
+age = st.slider(
+    " Enter your years of experience in Python Programming", 0, 30, 2)
+st.write(
+    f"wow you have {age} years of experience. After having {age} years experience in writing, give me a summary of the time you spent Programming")
+
+
+# Image
+
+image_path = "images/picture of plane1.webp"
+image = Image.open(image_path)
+
+# Fit image to the column width
+st.image(image, caption="Sample Image", use_container_width=True)
+
+
+# --- END SECTION ---
+st.markdown("""
+    <style>
+    .zero {
+        text-align: center;
+        padding: 50px 100px;
+        background: linear-gradient(135deg, #0ea5e9, #0369a1);
+        color: red;
+        border-radius: 10px;
+        margin-bottom: 50px;
+    }
+    .hero h1 {
+        font-size: 3em;
+        font-weight: 800;
+    }
+    .hero p {
+        font-size: 1.3em;
+        color: #e0f2fe;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+<div class="hero">
+    <h1> Thanks for Visiting my Web Application</h1>
+    <p> Programmed by Ilim.A.Theophilus</p>
+            <p> Creator of ASHMIS </p>
+</div>
+""", unsafe_allow_html=True)
